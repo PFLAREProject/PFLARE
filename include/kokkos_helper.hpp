5e348e59e2b3dc888cabbbdb4eaad6d5fec5f789
sdargavi <s.dargaville@imperial.ac.uk>
2025-03-28 22:07:20 +0000

    Wrong memory space

--- include/kokkos_helper.hpp
+++ include/kokkos_helper.hpp
@@ -20,7 +20,7 @@ using intKokkosViewHost = Kokkos::View<int *, HostMirrorMemorySpace>;
 using intKokkosView = Kokkos::View<int *, Kokkos::DefaultExecutionSpace>;
 using boolKokkosView = Kokkos::View<bool *, Kokkos::DefaultExecutionSpace>;
 using ConstMatRowMapKokkosView = KokkosCsrGraph::row_map_type::const_type;
-using ScratchMemSpace = Kokkos::ScratchMemorySpace<Kokkos::DefaultExecutionSpace>;
+using ScratchMemSpace = typename KokkosTeamMemberType::scratch_memory_space;
 
 PETSC_INTERN void mat_duplicate_copy_plus_diag_kokkos(Mat *, int, Mat *);
 
