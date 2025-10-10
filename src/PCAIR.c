e6b3fa84549673f75959809234a577a09e26eb3f
sdargavi <s.dargaville@imperial.ac.uk>
2025-10-10 05:57:09 +0100

    Wrong name to dynamic linker function

--- src/PCAIR.c
+++ src/PCAIR.c
@@ -1856,7 +1856,7 @@ PETSC_EXTERN void PCRegister_AIR()
 // petsc as a shared library - this enables --download-pflare in the petsc
 // configure to just work
 // Is unused if static linking
-PETSC_EXTERN PetscErrorCode PetscDLLibraryRegister_pflare(void)
+PETSC_EXTERN PetscErrorCode PetscDLLibraryRegister_petscpflare(void)
 {
   PetscFunctionBegin;
   PCRegister_AIR();
