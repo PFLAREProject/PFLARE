bd43b12444d9e7226be3f36c3ef24bb87d8d20e4
sdargavi <s.dargaville@imperial.ac.uk>
2026-03-04 17:35:09 +0000

    more printing

--- src/AIR_Operators_Setup.F90
+++ src/AIR_Operators_Setup.F90
@@ -957,6 +957,8 @@ module air_operators_setup
 
       PetscErrorCode :: ierr
       type(tMat) :: temp_mat
+      MPIU_Comm            :: MPI_COMM_MATRIX
+      integer :: comm_rank
 
       ! ~~~~~~~~~~
 
@@ -967,7 +969,11 @@ module air_operators_setup
       ! Z A_ff W + A_cf W + Z A_fc + A_cc
       ! ~~~~~~~~~~~~~~~~~~~
       ! ~~~~~~~~~~~~~~~~~~~
-      call timer_start(TIMER_ID_AIR_RAP)                          
+      call timer_start(TIMER_ID_AIR_RAP)
+      call PetscObjectGetComm(A, MPI_COMM_MATRIX, ierr)
+      ! Get the comm size 
+      ! Get the comm rank
+      call MPI_Comm_rank(MPI_COMM_MATRIX, comm_rank, errorcode)                                     
 
       ! Can just do PtAP
       if (air_data%options%symmetric) then  
@@ -1004,11 +1010,17 @@ module air_operators_setup
          ! First time
          else
 
+            print *, comm_rank, "AP start"
+
             call MatMatMult(A, air_data%prolongators(our_level), &
                      MAT_INITIAL_MATRIX, 1.58d0, air_data%reuse(our_level)%reuse_mat(MAT_AP), ierr)     
+
+            print *, comm_rank, "AP end"
                      
             call MatMatMult(air_data%restrictors(our_level), air_data%reuse(our_level)%reuse_mat(MAT_AP), &
                      MAT_INITIAL_MATRIX, 1.58d0, air_data%reuse(our_level)%reuse_mat(MAT_RAP), ierr) 
+
+            print *, comm_rank, "R(AP) end"
          end if
          
          ! Delete temporary if not reusing
@@ -1038,6 +1050,8 @@ module air_operators_setup
 
       ! First time so just drop according to a tolerance 
       else
+         print *, comm_rank, "remove small sparse start"
+
          ! If we know we're not reusing save a copy
          if (.NOT. air_data%options%reuse_sparsity) then
             call remove_small_from_sparse(air_data%reuse(our_level)%reuse_mat(MAT_RAP), &
@@ -1051,6 +1065,8 @@ module air_operators_setup
             call MatDuplicate(air_data%reuse(our_level)%reuse_mat(MAT_RAP_DROP), &
                         MAT_COPY_VALUES, coarse_matrix, ierr)
          end if
+
+         print *, comm_rank, "remove small sparse end"
       end if
 
       ! Delete temporary if not reusing
