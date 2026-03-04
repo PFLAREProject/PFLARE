99c4d548e82bdd954199f3dfb7b79684d8d1d0bc
sdargavi <s.dargaville@imperial.ac.uk>
2026-03-04 17:37:24 +0000

    fix

--- src/AIR_Operators_Setup.F90
+++ src/AIR_Operators_Setup.F90
@@ -958,7 +958,7 @@ module air_operators_setup
       PetscErrorCode :: ierr
       type(tMat) :: temp_mat
       MPIU_Comm            :: MPI_COMM_MATRIX
-      integer :: comm_rank
+      integer :: comm_rank, errorcode
 
       ! ~~~~~~~~~~
 
