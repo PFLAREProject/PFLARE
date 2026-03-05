PFLAREProject/PFLARE

commit bfb6f6b7ab3f3f2ba5f9bea15ec51557fbced852

Power and arnoldi GMRES polynomials can now use diagonal scaling option. Newton to come

src/Approx_Inverse_Setup.F90
@@ -20,7 +20,7 @@ module approx_inverse_setup
 
    subroutine calculate_and_build_approximate_inverse(matrix, inverse_type, &
                   poly_order, inverse_sparsity_order, &
-                  matrix_free, subcomm, &
+                  matrix_free, diag_scale_polys, subcomm, &
                   inv_matrix)
 
       ! Builds an approximate inverse
@@ -42,7 +42,7 @@ module approx_inverse_setup
       type(tMat), intent(in)                            :: matrix
       integer, intent(in)                               :: inverse_type, poly_order
       integer, intent(in)                               :: inverse_sparsity_order
-      logical, intent(in)                               :: matrix_free, subcomm
+      logical, intent(in)                               :: matrix_free, diag_scale_polys, subcomm
       type(tMat), intent(inout)                         :: inv_matrix
 
       type(tsqr_buffers)                           :: buffers 
@@ -100,17 +100,17 @@ module approx_inverse_setup
       ! but some methods benefit from having their intermediate calculations 
       ! done on a subcomm
       buffers%subcomm = subcomm
-      ! Don't do any re-use, if you want reuse call the start/finish yourself   
+      ! Don't do any overlapping of comms, if you want that call the start/finish yourself   
 
       ! Start the calculation
       call start_approximate_inverse(matrix, inverse_type, &
-                  poly_order, &
+                  poly_order, diag_scale_polys, &
                   buffers, coefficients)
       ! Finish it
       call finish_approximate_inverse(matrix, inverse_type, &
                   poly_order, inverse_sparsity_order, &
                   buffers, coefficients, &
-                  matrix_free, &
+                  matrix_free, diag_scale_polys, &
                   reuse_mat, &
                   reuse_submatrices, &
                   inv_matrix_temp)
@@ -131,7 +131,7 @@ module approx_inverse_setup
 
 ! -------------------------------------------------------------------------------------------------------------------------------
 
-   subroutine start_approximate_inverse(matrix, inverse_type, poly_order, &
+   subroutine start_approximate_inverse(matrix, inverse_type, poly_order, diag_scale_polys, &
                   buffers, coefficients)
 
       ! Starts the assembly of an approximate inverse
@@ -139,16 +139,17 @@ module approx_inverse_setup
       ! Have to call finish_approximate_inverse before it can be used
 
       ! ~~~~~~
-      type(tMat), intent(in)                            :: matrix
-      integer, intent(in)                               :: inverse_type, poly_order
-      type(tsqr_buffers), intent(inout)                 :: buffers 
-      PetscReal, dimension(:, :), pointer, intent(inout)     :: coefficients
+      type(tMat), intent(in)                             :: matrix
+      integer, intent(in)                                :: inverse_type, poly_order
+      logical, intent(in)                                :: diag_scale_polys
+      type(tsqr_buffers), intent(inout)                  :: buffers 
+      PetscReal, dimension(:, :), pointer, intent(inout) :: coefficients
 
       PetscErrorCode :: ierr
-      MPIU_Comm :: MPI_COMM_MATRIX
+      MPIU_Comm :: MPI_COMM_MATRIX, MPI_COMM_BUFFERS_MATRIX
+      PetscInt :: local_rows, local_cols, global_rows, global_cols
       integer :: errorcode
-      type(tMat) :: diag_mat
-      type(tVec) :: diag_vec
+      type(mat_ctxtype), pointer :: mat_ctx_da=>null()
       ! ~~~~~~    
 
       if (buffers%subcomm .AND. inverse_type == PFLAREINV_POWER) then
@@ -180,22 +181,63 @@ module approx_inverse_setup
          buffers%matrix = matrix
          ! Increase the reference counter
          call PetscObjectReference(matrix, ierr)          
-      end if
+      end if     
 
       ! ~~~~~~~~~~~~~~~
       ! ~~~~~~~~~~~~~~~
 
-         ! ! Need to build an assembled D^-1 A
-         ! call MatDuplicate(buffers%matrix, MAT_COPY_VALUES, diag_mat, ierr)
-         ! buffers%matrix = diag_mat
-         ! call MatCreateVecs(buffers%matrix, PETSC_NULL_VEC, diag_vec, ierr)
-         ! call MatGetDiagonal(buffers%matrix, diag_vec, ierr)
-         ! call VecReciprocal(diag_vec, ierr)
-         ! call MatDiagonalScale(buffers%matrix, diag_vec, PETSC_NULL_VEC, ierr) 
-
+      ! Have to dynamically allocate this
+      allocate(mat_ctx_da)
+      mat_ctx_da%mat_scaled = buffers%matrix
 
       ! If we're on the subcomm, we don't need to do anything
       if (.NOT. PetscObjectIsNull(buffers%matrix)) then
+
+         ! If we want to diagonally scale (ie apply Jacobi preconditioned gmres polynomial)
+         if ((inverse_type == PFLAREINV_POWER .OR. &
+             inverse_type == PFLAREINV_ARNOLDI .OR. &
+             inverse_type == PFLAREINV_NEWTON .OR. &
+             inverse_type == PFLAREINV_NEWTON_NO_EXTRA) .AND. &
+             diag_scale_polys) then
+
+            ! ~~~~~~~~~~~~~
+            ! Now we allocate a new matshell that applies a diagonally scaled version of 
+            ! the matrix to compute the coefficients
+            ! ~~~~~~~~~~~~~       
+            ! Get the comm on the buffers matrix (which may be a subcomm)
+            call PetscObjectGetComm(buffers%matrix, MPI_COMM_BUFFERS_MATRIX, ierr)                
+      
+            ! Get the sizes
+            call MatGetLocalSize(buffers%matrix, local_rows, local_cols, ierr)
+            call MatGetSize(buffers%matrix, global_rows, global_cols, ierr)                
+            
+            ! Create the matshell
+            call MatCreateShell(MPI_COMM_BUFFERS_MATRIX, local_rows, local_cols, global_rows, global_cols, &
+                        mat_ctx_da, mat_ctx_da%mat_scaled, ierr)
+            ! The subroutine petsc_matvec_da_poly_mf applies D^-1 A
+            call MatShellSetOperation(mat_ctx_da%mat_scaled, &
+                        MATOP_MULT, petsc_matvec_da_poly_mf, ierr)
+
+            call MatAssemblyBegin(mat_ctx_da%mat_scaled, MAT_FINAL_ASSEMBLY, ierr)
+            call MatAssemblyEnd(mat_ctx_da%mat_scaled, MAT_FINAL_ASSEMBLY, ierr)   
+            ! Have to make sure to set the type of vectors the shell creates
+            call ShellSetVecType(buffers%matrix, mat_ctx_da%mat_scaled)   
+            
+            ! Create temporary vector we use during horner
+            ! Make sure to use matrix here to get the right type (as the shell doesn't know about gpus)            
+            call MatCreateVecs(buffers%matrix, PETSC_NULL_VEC, mat_ctx_da%mf_temp_vec(MF_VEC_DIAG), ierr) 
+            ! Get the diagonal
+            call MatGetDiagonal(buffers%matrix, mat_ctx_da%mf_temp_vec(MF_VEC_DIAG), ierr)    
+            ! This is the matrix whose inverse we are applying (just copying the pointer here)
+            mat_ctx_da%mat = buffers%matrix
+
+         end if
+
+         ! ~~~~~~~~~
+         ! mat_ctx_da%mat_scaled now contains a shell that just does D^-1 A
+         ! We compute the coefficients with that and then finish_approximate_inverse builds
+         ! whatever matrix it needs to compute the appropriate inverse with those coeffs
+         ! ~~~~~~~~~
 
          ! Gmres poylnomial with power basis
          if (inverse_type == PFLAREINV_POWER) then
@@ -203,26 +245,38 @@ module approx_inverse_setup
             ! Only does one reduction in parallel
             ! Want to start the coefficient calculation asap, as we have a non-blocking
             ! all reduce in it
-            call start_gmres_polynomial_coefficients_power(buffers%matrix, poly_order, &
+            call start_gmres_polynomial_coefficients_power(mat_ctx_da%mat_scaled, poly_order, &
                   buffers, coefficients(:, 1))         
 
          ! Gmres polynomial with arnoldi basis
          else if (inverse_type == PFLAREINV_ARNOLDI) then
 
             ! Does lots of reductions in parallel
-            call calculate_gmres_polynomial_coefficients_arnoldi(buffers%matrix, poly_order, coefficients(:, 1))
+            call calculate_gmres_polynomial_coefficients_arnoldi(mat_ctx_da%mat_scaled, poly_order, coefficients(:, 1))
 
          ! Gmres polynomial with Newton basis with extra added roots for stability - Can only use matrix-free
          else if (inverse_type == PFLAREINV_NEWTON) then
 
             ! Does lots of reductions in parallel
-            call calculate_gmres_polynomial_roots_newton(buffers%matrix, poly_order, .TRUE., coefficients)
+            call calculate_gmres_polynomial_roots_newton(mat_ctx_da%mat_scaled, poly_order, .TRUE., coefficients)
 
          ! Gmres polynomial with Newton basis without extra added roots - Can only use matrix-free
          else if (inverse_type == PFLAREINV_NEWTON_NO_EXTRA) then
 
             ! Does lots of reductions in parallel
-            call calculate_gmres_polynomial_roots_newton(buffers%matrix, poly_order, .FALSE., coefficients)            
+            call calculate_gmres_polynomial_roots_newton(mat_ctx_da%mat_scaled, poly_order, .FALSE., coefficients)            
+         end if
+
+         ! Destroy the shell matrix
+         if ((inverse_type == PFLAREINV_POWER .OR. &
+             inverse_type == PFLAREINV_ARNOLDI .OR. &
+             inverse_type == PFLAREINV_NEWTON .OR. &
+             inverse_type == PFLAREINV_NEWTON_NO_EXTRA) .AND. &
+             diag_scale_polys) then
+
+            call VecDestroy(mat_ctx_da%mf_temp_vec(MF_VEC_DIAG), ierr)
+            call MatDestroy(mat_ctx_da%mat_scaled, ierr)
+            deallocate(mat_ctx_da)               
          end if
       end if
 
@@ -265,7 +319,7 @@ module approx_inverse_setup
    subroutine finish_approximate_inverse(matrix, inverse_type, &
                   poly_order, inverse_sparsity_order, &
                   buffers, coefficients, &
-                  matrix_free, reuse_mat, reuse_submatrices, inv_matrix)
+                  matrix_free, diag_scale_polys, reuse_mat, reuse_submatrices, inv_matrix)
 
       ! Finish the assembly of an approximate inverse
 
@@ -275,7 +329,7 @@ module approx_inverse_setup
       integer, intent(in)                               :: inverse_sparsity_order
       type(tsqr_buffers), intent(inout)                 :: buffers      
       PetscReal, dimension(:, :), pointer, contiguous, intent(inout) :: coefficients
-      logical, intent(in)                               :: matrix_free
+      logical, intent(in)                               :: matrix_free, diag_scale_polys
       type(tMat), intent(inout)                         :: reuse_mat, inv_matrix
       type(tMat), dimension(:), pointer, intent(inout)  :: reuse_submatrices
 
@@ -287,7 +341,6 @@ module approx_inverse_setup
 #else
       MPIU_Status, dimension(MPI_STATUS_SIZE) :: status
 #endif
-      type(tVec) :: diag_vec, diag_inverse_vec
 
       ! ~~~~~~    
 
@@ -313,10 +366,10 @@ module approx_inverse_setup
       ! Gmres poylnomial with power or arnoldi basis
       if (inverse_type == PFLAREINV_POWER .OR. inverse_type == PFLAREINV_ARNOLDI) then
 
-         ! needs to be buffers%matrix to work with diag scaling NOW @@@@
          call build_gmres_polynomial_inverse(matrix, poly_order, &
                   buffers, coefficients(:, 1), &
-                  inverse_sparsity_order, matrix_free, reuse_mat, reuse_submatrices, inv_matrix)  
+                  inverse_sparsity_order, matrix_free, diag_scale_polys, &
+                  reuse_mat, reuse_submatrices, inv_matrix)  
 
       ! Gmres polynomial with newton basis
       else if (inverse_type == PFLAREINV_NEWTON .OR. inverse_type == PFLAREINV_NEWTON_NO_EXTRA) then
@@ -365,12 +418,6 @@ module approx_inverse_setup
 
       end if
 
-         ! call MatCreateVecs(matrix, diag_inverse_vec, diag_vec, ierr)
-         ! call MatGetDiagonal(matrix, diag_vec, ierr)
-         ! call VecCopy(diag_vec, diag_inverse_vec, ierr)
-         ! call VecReciprocal(diag_inverse_vec, ierr)
-         ! call MatDiagonalScale(inv_matrix, PETSC_NULL_VEC, diag_inverse_vec, ierr)       
-
       ! If we were on a subcomm we created a copy of our matrix, if not we incremented 
       ! the reference counter
       call MatDestroy(buffers%matrix, ierr)            
@@ -388,7 +435,7 @@ module approx_inverse_setup
 
       PetscErrorCode :: ierr
       MatType:: mat_type
-      type(mat_ctxtype), pointer :: mat_ctx=>null(), mat_ctx_ida=>null()
+      type(mat_ctxtype), pointer :: mat_ctx=>null(), mat_ctx_scaled=>null()
       ! ~~~~~~
 
       if (.NOT. PetscObjectIsNull(matrix)) then
@@ -403,7 +450,7 @@ module approx_inverse_setup
             call VecDestroy(mat_ctx%mf_temp_vec(MF_VEC_TEMP), ierr)
 
             ! Both newton and neumann polynomials use some extra temporary vectors
-            if (.NOT. PetscObjectIsNull(mat_ctx%mat_ida) .OR. &
+            if (.NOT. PetscObjectIsNull(mat_ctx%mat_scaled) .OR. &
                      associated(mat_ctx%real_roots)) then
                
                call VecDestroy(mat_ctx%mf_temp_vec(MF_VEC_RHS), ierr)
@@ -411,11 +458,10 @@ module approx_inverse_setup
             end if
 
             ! Neumann polynomial has extra context that needs deleting
-            if (.NOT. PetscObjectIsNull(mat_ctx%mat_ida)) then
-               call MatShellGetContext(mat_ctx%mat_ida, mat_ctx_ida, ierr)
-               deallocate(mat_ctx_ida)
-               call MatDestroy(mat_ctx%mat_ida, ierr)
-
+            if (.NOT. PetscObjectIsNull(mat_ctx%mat_scaled)) then
+               call MatShellGetContext(mat_ctx%mat_scaled, mat_ctx_scaled, ierr)
+               deallocate(mat_ctx_scaled)
+               call MatDestroy(mat_ctx%mat_scaled, ierr)
             end if
             deallocate(mat_ctx)
          end if               

include/pflare.h
@@ -73,7 +73,7 @@ PETSC_EXTERN PetscErrorCode PCAIRGetDDCFraction(PC, PetscReal *);
 PETSC_EXTERN PetscErrorCode PCAIRGetCFSplittingType(PC, CFSplittingType *);
 PETSC_EXTERN PetscErrorCode PCAIRGetMaxLubySteps(PC, PetscInt *);
 PETSC_EXTERN PetscErrorCode PCAIRGetSmoothType(PC, char *);
-PETSC_EXTERN PetscErrorCode PCAIRGetPolyDiagScale(PC, PetscBool *);
+PETSC_EXTERN PetscErrorCode PCAIRGetDiagScalePolys(PC, PetscBool *);
 PETSC_EXTERN PetscErrorCode PCAIRGetMatrixFreePolys(PC, PetscBool *);
 PETSC_EXTERN PetscErrorCode PCAIRGetOnePointClassicalProlong(PC, PetscBool *);
 PETSC_EXTERN PetscErrorCode PCAIRGetFullSmoothingUpAndDown(PC, PetscBool *);
@@ -120,7 +120,7 @@ PETSC_EXTERN PetscErrorCode PCAIRSetDDCFraction(PC, PetscReal);
 PETSC_EXTERN PetscErrorCode PCAIRSetCFSplittingType(PC, CFSplittingType);
 PETSC_EXTERN PetscErrorCode PCAIRSetMaxLubySteps(PC, PetscInt);
 PETSC_EXTERN PetscErrorCode PCAIRSetSmoothType(PC, const char *);
-PETSC_EXTERN PetscErrorCode PCAIRSetPolyDiagScale(PC, PetscBool);
+PETSC_EXTERN PetscErrorCode PCAIRSetDiagScalePolys(PC, PetscBool);
 PETSC_EXTERN PetscErrorCode PCAIRSetMatrixFreePolys(PC, PetscBool);
 PETSC_EXTERN PetscErrorCode PCAIRSetOnePointClassicalProlong(PC, PetscBool);
 PETSC_EXTERN PetscErrorCode PCAIRSetFullSmoothingUpAndDown(PC, PetscBool);

src/AIR_Data_Type.F90
@@ -121,8 +121,8 @@ module air_data_type
       ! Do we do any C point smooths?
       logical :: any_c_smooths = .FALSE.
       ! If using a polynomial inverse type, do we diagonally scale before computing?
-      ! -pc_air_poly_diag_scale
-      logical :: poly_diag_scale = .FALSE.      
+      ! -pc_air_diag_scale_polys
+      logical :: diag_scale_polys = .FALSE.      
       ! Do we apply our polynomials matrix free when smoothing?
       ! -pc_air_matrix_free_polys
       logical :: matrix_free_polys = .FALSE.

src/AIR_Data_Type_Routines.F90
@@ -283,7 +283,7 @@ module air_data_type_routines
       air_data%options%smooth_order = 0
       air_data%options%smooth_order(1) = 2
       air_data%options%any_c_smooths = .FALSE.
-      air_data%options%poly_diag_scale = .FALSE.
+      air_data%options%diag_scale_polys = .FALSE.
       air_data%options%matrix_free_polys = .FALSE.
       air_data%options%one_point_classical_prolong = .TRUE.
       air_data%options%full_smoothing_up_and_down = .FALSE.

src/AIR_MG_Setup.F90
@@ -154,6 +154,7 @@ module air_mg_setup
             call start_approximate_inverse(air_data%coarse_matrix(our_level), &
                   air_data%inv_coarsest_poly_data%inverse_type, &
                   air_data%inv_coarsest_poly_data%gmres_poly_order, &
+                  .FALSE., &
                   air_data%inv_coarsest_poly_data%buffers, &
                   air_data%inv_coarsest_poly_data%coefficients)                       
 
@@ -178,6 +179,7 @@ module air_mg_setup
                   air_data%inv_coarsest_poly_data%buffers, &
                   air_data%inv_coarsest_poly_data%coefficients, &
                   air_data%options%coarsest_matrix_free_polys, &
+                  .FALSE., &
                   air_data%reuse(our_level)%reuse_mat(MAT_INV_AFF), &
                   air_data%reuse(our_level)%reuse_submatrices(MAT_INV_AFF)%array, &
                   air_data%inv_A_ff(our_level))          
@@ -894,6 +896,7 @@ module air_mg_setup
             call start_approximate_inverse(air_data%coarse_matrix(no_levels), &
                   air_data%inv_coarsest_poly_data%inverse_type, &
                   air_data%inv_coarsest_poly_data%gmres_poly_order, &
+                  .FALSE., &
                   air_data%inv_coarsest_poly_data%buffers, &
                   air_data%inv_coarsest_poly_data%coefficients)         
             call timer_finish(TIMER_ID_AIR_INVERSE)
@@ -1058,6 +1061,7 @@ module air_mg_setup
                   air_data%inv_coarsest_poly_data%buffers, &
                   air_data%inv_coarsest_poly_data%coefficients, &
                   air_data%options%coarsest_matrix_free_polys, &
+                  .FALSE., &
                   air_data%reuse(air_data%no_levels)%reuse_mat(MAT_INV_AFF), &
                   air_data%reuse(air_data%no_levels)%reuse_submatrices(MAT_INV_AFF)%array, &
                   air_data%inv_A_ff(air_data%no_levels))           

src/AIR_Operators_Setup.F90
@@ -109,6 +109,7 @@ module air_operators_setup
          call start_approximate_inverse(smoothing_mat, &
                   air_data%inv_A_ff_poly_data(our_level)%inverse_type, &
                   air_data%inv_A_ff_poly_data(our_level)%gmres_poly_order, &
+                  air_data%options%diag_scale_polys, &
                   air_data%inv_A_ff_poly_data(our_level)%buffers, &
                   air_data%inv_A_ff_poly_data(our_level)%coefficients)        
       end if
@@ -126,6 +127,7 @@ module air_operators_setup
             call start_approximate_inverse(air_data%reuse(our_level)%reuse_mat(MAT_AFF_DROP), &
                   air_data%inv_A_ff_poly_data_dropped(our_level)%inverse_type, &
                   air_data%inv_A_ff_poly_data_dropped(our_level)%gmres_poly_order, &
+                  air_data%options%diag_scale_polys, &
                   air_data%inv_A_ff_poly_data_dropped(our_level)%buffers, &
                   air_data%inv_A_ff_poly_data_dropped(our_level)%coefficients)          
          end if
@@ -159,6 +161,7 @@ module air_operators_setup
             call start_approximate_inverse(air_data%A_cc(our_level), &
                   air_data%inv_A_cc_poly_data(our_level)%inverse_type, &
                   air_data%inv_A_cc_poly_data(our_level)%gmres_poly_order, &
+                  air_data%options%diag_scale_polys, &
                   air_data%inv_A_cc_poly_data(our_level)%buffers, &
                   air_data%inv_A_cc_poly_data(our_level)%coefficients)  
          end if
@@ -352,7 +355,9 @@ module air_operators_setup
             air_data%inv_A_ff_poly_data(our_level)%gmres_poly_sparsity_order, &
             air_data%inv_A_ff_poly_data(our_level)%buffers, &
             air_data%inv_A_ff_poly_data(our_level)%coefficients, &
-            air_data%options%matrix_free_polys, air_data%reuse(our_level)%reuse_mat(MAT_INV_AFF), &
+            air_data%options%matrix_free_polys, &
+            air_data%options%diag_scale_polys, &
+            air_data%reuse(our_level)%reuse_mat(MAT_INV_AFF), &
             air_data%reuse(our_level)%reuse_submatrices(MAT_INV_AFF)%array, &
             air_data%inv_A_ff(our_level)) 
             
@@ -382,7 +387,9 @@ module air_operators_setup
                      air_data%inv_A_ff_poly_data_dropped(our_level)%gmres_poly_sparsity_order, &
                      air_data%inv_A_ff_poly_data_dropped(our_level)%buffers, &
                      air_data%inv_A_ff_poly_data_dropped(our_level)%coefficients, &
-                     .FALSE., air_data%reuse(our_level)%reuse_mat(MAT_INV_AFF_DROPPED), &
+                     .FALSE., &
+                     air_data%options%diag_scale_polys, &
+                     air_data%reuse(our_level)%reuse_mat(MAT_INV_AFF_DROPPED), &
                      air_data%reuse(our_level)%reuse_submatrices(MAT_INV_AFF_DROPPED)%array, &
                      inv_dropped_Aff)
             destroy_mat = .TRUE.
@@ -409,7 +416,9 @@ module air_operators_setup
                         air_data%inv_A_ff_poly_data(our_level)%gmres_poly_sparsity_order, &
                         air_data%inv_A_ff_poly_data(our_level)%buffers, &
                         air_data%inv_A_ff_poly_data(our_level)%coefficients, &
-                        .FALSE., air_data%reuse(our_level)%reuse_mat(MAT_INV_AFF_DROPPED), &
+                        .FALSE., &
+                        air_data%options%diag_scale_polys, &
+                        air_data%reuse(our_level)%reuse_mat(MAT_INV_AFF_DROPPED), &
                         air_data%reuse(our_level)%reuse_submatrices(MAT_INV_AFF_DROPPED)%array, &
                         inv_dropped_Aff)
                destroy_mat = .TRUE.
@@ -909,6 +918,7 @@ module air_operators_setup
                   air_data%inv_A_cc_poly_data(our_level)%buffers, &
                   air_data%inv_A_cc_poly_data(our_level)%coefficients, &
                   air_data%options%matrix_free_polys, &
+                  air_data%options%diag_scale_polys, &
                   air_data%reuse(our_level)%reuse_mat(MAT_INV_ACC), &
                   air_data%reuse(our_level)%reuse_submatrices(MAT_INV_ACC)%array, &
                   air_data%inv_A_cc(our_level))

src/C_Fortran_Bindings.F90
@@ -94,7 +94,7 @@ module c_fortran_bindings
 
    subroutine calculate_and_build_approximate_inverse_c(input_mat_ptr, inverse_type, &
          poly_order, poly_sparsity_order, &
-         matrix_free_int, subcomm_int, inv_matrix_ptr) &
+         matrix_free_int, diag_scale_polys_int, subcomm_int, inv_matrix_ptr) &
          bind(C,name='calculate_and_build_approximate_inverse_c')
 
       ! Builds an approximate inverse
@@ -102,11 +102,11 @@ module c_fortran_bindings
       ! ~~~~~~~~
       integer(c_long_long), intent(in)       :: input_mat_ptr
       integer(c_int), value, intent(in)      :: inverse_type, poly_order, poly_sparsity_order
-      integer(c_int), value, intent(in)      :: matrix_free_int, subcomm_int
+      integer(c_int), value, intent(in)      :: matrix_free_int, diag_scale_polys_int, subcomm_int
       integer(c_long_long), intent(inout)    :: inv_matrix_ptr
 
       type(tMat)  :: input_mat, inv_matrix
-      logical     :: matrix_free = .FALSE., subcomm = .FALSE.
+      logical     :: matrix_free = .FALSE., subcomm = .FALSE., diag_scale_polys = .FALSE.
       ! ~~~~~~~~      
       
       ! Now the input mat long long just gets copied into input_mat%v
@@ -118,10 +118,11 @@ module c_fortran_bindings
       inv_matrix%v = inv_matrix_ptr
       
       if (matrix_free_int == 1) matrix_free = .TRUE.
+      if (diag_scale_polys_int == 1) diag_scale_polys = .TRUE.
       if (subcomm_int == 1) subcomm = .TRUE.
       call calculate_and_build_approximate_inverse(input_mat, inverse_type, &
                poly_order, poly_sparsity_order, &
-               matrix_free, subcomm, &
+               matrix_free, diag_scale_polys, subcomm, &
                inv_matrix)
 
       ! Pass out the new inverse matrix

commit e3b0bc039826b0fe6bc31cb71d181c2aeb687dd8

Fix memory leak introduced in bfb6f6b7ab3f3f2ba5f9bea15ec51557fbced852

src/Approx_Inverse_Setup.F90
@@ -254,19 +254,19 @@ module approx_inverse_setup
       ! ~~~~~~~~~~~~~~~
       ! ~~~~~~~~~~~~~~~
 
-      ! Have to dynamically allocate this
-      allocate(mat_ctx_da)
-      mat_ctx_da%mat_scaled = buffers%matrix
-
       ! If we're on the subcomm, we don't need to do anything
       if (.NOT. PetscObjectIsNull(buffers%matrix)) then
+
+         ! Have to dynamically allocate this
+         allocate(mat_ctx_da)
+         mat_ctx_da%mat_scaled = buffers%matrix           
 
          ! If we want to diagonally scale (ie apply Jacobi preconditioned gmres polynomial)
          if ((inverse_type == PFLAREINV_POWER .OR. &
              inverse_type == PFLAREINV_ARNOLDI .OR. &
              inverse_type == PFLAREINV_NEWTON .OR. &
              inverse_type == PFLAREINV_NEWTON_NO_EXTRA) .AND. &
-             diag_scale_polys) then
+             diag_scale_polys) then             
 
             ! ~~~~~~~~~~~~~
             ! Now we allocate a new matshell that applies a diagonally scaled version of 
@@ -342,10 +342,10 @@ module approx_inverse_setup
              inverse_type == PFLAREINV_NEWTON_NO_EXTRA) .AND. &
              diag_scale_polys) then
 
-            call VecDestroy(mat_ctx_da%mf_temp_vec(MF_VEC_DIAG), ierr)
+            call VecDestroy(mat_ctx_da%mf_temp_vec(MF_VEC_DIAG), ierr)             
             call MatDestroy(mat_ctx_da%mat_scaled, ierr)
-            deallocate(mat_ctx_da)               
          end if
+         deallocate(mat_ctx_da)           
       end if
 
       ! If we ended up on a subcomm, we need to comm the coefficients back to 