d632dc1bf7cfc6125ee8fdbfcb8b0aff6d12f9c1
sdargavi <s.dargaville@imperial.ac.uk>
2026-03-13 04:14:16 +0000

    Update the test of calling compute_cf_splitting by itself to actually test the CF splitting is valid and added a case where it sets max_dd_ratio, as that triggers a different code path

--- tests/ex6_cf_splitting.c
+++ tests/ex6_cf_splitting.c
@@ -1,10 +1,77 @@
-static char help[] = "Reads a PETSc matrix and vector from a file and solves a linear system.\n\
+static char help[] = "Checks we can read in a linear system and compute a CF splitting.\n\
 Input arguments are:\n\
   -f <input_file> : file to load. For example see $PETSC_DIR/share/petsc/datafiles/matrices\n\n";
 
 #include <petscksp.h>
 #include <petsclog.h>
 #include "pflare.h"
+
+// Check that a given CF splitting is valid
+static PetscErrorCode CheckSplitting(Mat A, IS is_fine, IS is_coarse, const char *label)
+{
+  PetscInt        n_fine_local, n_coarse_local;
+  PetscInt        n_fine_global, n_coarse_global;
+  PetscInt        local_rows, local_cols;
+  PetscInt        global_rows, global_cols;
+  PetscInt        rstart, rend, i;
+  PetscInt        nlocal;
+  const PetscInt  *idx_fine, *idx_coarse;
+  PetscInt        *seen;
+  PetscMPIInt     rank;
+
+  PetscFunctionBeginUser;
+  PetscCallMPI(MPI_Comm_rank(PETSC_COMM_WORLD, &rank));
+
+  PetscCall(MatGetLocalSize(A, &local_rows, &local_cols));
+  PetscCall(MatGetSize(A, &global_rows, &global_cols));
+  PetscCall(MatGetOwnershipRange(A, &rstart, &rend));
+
+  PetscCall(ISGetLocalSize(is_fine, &n_fine_local));
+  PetscCall(ISGetLocalSize(is_coarse, &n_coarse_local));
+  PetscCall(ISGetSize(is_fine, &n_fine_global));
+  PetscCall(ISGetSize(is_coarse, &n_coarse_global));
+
+  PetscCheck(n_fine_local + n_coarse_local == local_rows, PETSC_COMM_SELF, PETSC_ERR_PLIB,
+             "%s: local size mismatch on rank %d: n_fine_local (%" PetscInt_FMT ") + n_coarse_local (%" PetscInt_FMT ") != local_rows (%" PetscInt_FMT ")",
+             label, rank, n_fine_local, n_coarse_local, local_rows);
+  PetscCheck(n_fine_global + n_coarse_global == global_rows, PETSC_COMM_SELF, PETSC_ERR_PLIB,
+             "%s: global size mismatch: n_fine_global (%" PetscInt_FMT ") + n_coarse_global (%" PetscInt_FMT ") != global_rows (%" PetscInt_FMT ")",
+             label, n_fine_global, n_coarse_global, global_rows);
+
+  PetscCall(PetscCalloc1(local_rows, &seen));
+
+  PetscCall(ISGetLocalSize(is_fine, &nlocal));
+  PetscCall(ISGetIndices(is_fine, &idx_fine));
+  for (i = 0; i < nlocal; i++) {
+    PetscCheck(idx_fine[i] >= rstart && idx_fine[i] < rend, PETSC_COMM_SELF, PETSC_ERR_PLIB,
+               "%s: fine index %" PetscInt_FMT " is not in local row ownership range [%" PetscInt_FMT ", %" PetscInt_FMT ") on rank %d",
+               label, idx_fine[i], rstart, rend, rank);
+    seen[idx_fine[i] - rstart]++;
+  }
+  PetscCall(ISRestoreIndices(is_fine, &idx_fine));
+
+  PetscCall(ISGetLocalSize(is_coarse, &nlocal));
+  PetscCall(ISGetIndices(is_coarse, &idx_coarse));
+  for (i = 0; i < nlocal; i++) {
+    PetscCheck(idx_coarse[i] >= rstart && idx_coarse[i] < rend, PETSC_COMM_SELF, PETSC_ERR_PLIB,
+               "%s: coarse index %" PetscInt_FMT " is not in local row ownership range [%" PetscInt_FMT ", %" PetscInt_FMT ") on rank %d",
+               label, idx_coarse[i], rstart, rend, rank);
+    seen[idx_coarse[i] - rstart]++;
+  }
+  PetscCall(ISRestoreIndices(is_coarse, &idx_coarse));
+
+  for (i = 0; i < local_rows; i++) {
+    PetscCheck(seen[i] == 1, PETSC_COMM_SELF, PETSC_ERR_PLIB,
+               "%s: owned row %" PetscInt_FMT " appears %" PetscInt_FMT " times in local fine/coarse IS on rank %d",
+               label, rstart + i, seen[i], rank);
+  }
+
+  PetscCall(PetscFree(seen));
+
+  if (!rank) PetscCall(PetscPrintf(PETSC_COMM_SELF, "%s: OK\n", label));
+
+  PetscFunctionReturn(PETSC_SUCCESS);
+}
 
 int main(int argc,char **args)
 {
@@ -102,30 +169,43 @@ int main(int argc,char **args)
      max_dd_ratio, \
      &is_fine, &is_coarse);
 
-  PetscInt n_fine, n_coarse;
-  PetscInt local_rows, local_cols;
-  PetscCall(MatGetLocalSize(A, &local_rows, &local_cols));
-  PetscCall(ISGetLocalSize(is_fine, &n_fine));
-  PetscCall(ISGetLocalSize(is_coarse, &n_coarse));
+  PetscCall(CheckSplitting(A, is_fine, is_coarse, "default max_dd_ratio"));
 
-  if (n_fine + n_coarse == local_rows)
-  {
-   PetscCall(PetscPrintf(PETSC_COMM_WORLD, "OK \n"));
-  }
-  else{
-   PetscCall(PetscPrintf(PETSC_COMM_WORLD, "NOT OK \n"));
-   return 1;
-  }
+  PetscCall(ISDestroy(&is_fine));
+  PetscCall(ISDestroy(&is_coarse));  
 
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  //       Compute a cf splitting with max_dd_ratio set
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
+
+ // This will keep doing ddc until this diagonal dominance
+ // ratio is hit
+ max_dd_ratio = 0.5;
+
+ compute_cf_splitting(A, \
+     symmetric, \
+     strong_threshold, max_luby_steps, \
+     algorithm, \
+     ddc_its, \
+     ddc_fraction, \
+     max_dd_ratio, \
+     &is_fine, &is_coarse);
+
+  PetscCall(CheckSplitting(A, is_fine, is_coarse, "max_dd_ratio=0.5"));
+
+  PetscCall(ISDestroy(&is_fine));
+  PetscCall(ISDestroy(&is_coarse));  
+
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
 
   /* Cleanup */
   PetscCall(VecDestroy(&x));
   PetscCall(VecDestroy(&b));
   PetscCall(VecDestroy(&u));
   PetscCall(MatDestroy(&A));
-  PetscCall(ISDestroy(&is_fine));
-  PetscCall(ISDestroy(&is_coarse));
+
   PetscCall(PetscFinalize());
   return 0;
 }
